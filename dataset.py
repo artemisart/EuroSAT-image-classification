3a98146a15487638b0fc9f806daf949492fb0de3
Mathis Chenuet <mathis.chenuet@etu.utc.fr>
2019-11-29 19:16:33 +0800

    add random dataset splitter

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from torchvision.datasets import ImageFolder
 from torchvision.datasets.utils import check_integrity, download_and_extract_archive
 
@@ -8,12 +9,24 @@ MD5 = "c8fa014336c82ac7804f0398fcb19387"
 SUBDIR = '2750'
 
 
+def random_split(dataset, ratio=0.9, random_state=None):
+    if random_state is not None:
+        state = torch.random.get_rng_state()
+        torch.random.manual_seed(random_state)
+    n = int(len(dataset) * ratio)
+    split = torch.utils.data.random_split(dataset, [n, len(dataset) - n])
+    if random_state is not None:
+        torch.random.set_rng_state(state)
+    return split
+
+
 class EuroSAT(ImageFolder):
     def __init__(self, root='data', transform=None, target_transform=None):
         self.download(root)
         root = os.path.join(root, SUBDIR)
         super().__init__(root, transform=transform, target_transform=target_transform)
 
-    def download(self, root):
+    @staticmethod
+    def download(root):
         if not check_integrity(os.path.join(root, "EuroSAT.zip")):
             download_and_extract_archive(URL, root, md5=MD5)
